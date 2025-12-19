f6df830029eadda50d9e3de4700d0fcb143cda03
DEEKSHA2005 <deeksha2235@gmail.com>
2025-12-19 12:54:38 +0530

    [Deeksha] : added Arrays and ControlFlows and Methods and ProgrammingElements

---
+++ src/CoreProgramming/ProgrammingElements/practice/Welcome.java
@@ -0,0 +1,8 @@
+package CoreProgramming.ProgrammingElements.practice;
+
+public class Welcome {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Bridgelabz!");
+    }
+}
