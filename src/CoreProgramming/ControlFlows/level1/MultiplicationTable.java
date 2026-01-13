4a4121bbfff9234f9ff0a0bc0287499ebb44f3e7
Deeksha <smdeeksha2005@gmail.com>
2026-01-13 16:33:04 +0530

    [Deeksha] : renamed packages

--- src/CoreProgramming/ControlFlows/level1/MultiplicationTable.java
+++ src/CoreProgramming/ControlFlows/level1/MultiplicationTable.java
@@ -2,7 +2,8 @@ package coreprogramming.controlflows.level1;
 
 import java.util.Scanner;
 
-public class MultiplicationTable {
+public class
+MultiplicationTable {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
