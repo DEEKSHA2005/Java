abcbff991d477d9850f0fbe412eb95decda7098e
Deeksha S M <115531476+DEEKSHA2005@users.noreply.github.com>
2026-01-13 16:05:54 +0530

    Delete src/coreprogramming/programmingelements/practice directory

--- src/coreprogramming/programmingelements/practice/Welcome.java
+++
@@ -1,8 +0,0 @@
-package coreprogramming.programmingelements.practice;
-
-public class Welcome {
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to Bridgelabz!");
-    }
-}
